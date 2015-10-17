4c0ab81cf50534c446077fc562c62c05c9f4df86
あまみや ゆうこ <1@1234.sh>
2015-10-17 18:31:30 +0800

    NugetPublishProvider

---
+++ src/CodeComb.CI.Publisher.NuGet/NuGetPublishProvider.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
+using CodeComb.Package;
+
+namespace CodeComb.CI.Publisher.NuGet
+{
+    public class NuGetPublishProvider : IPublishProvider
+    {
+        public NuGetPublishProvider() { }
+
+        public NuGetPublishProvider(string path, string apiKey, string fileRules = "*.nupkg", string address = "https://www.nuget.org/")
+        {
+            ApiKey = apiKey;
+            FileRules = fileRules;
+            Address = address;
+            Path = path;
+        }
+
+        public string Address { get; set; }
+
+        public string ApiKey { get; set; }
+
+        public string FileRules { get; set; }
+
+        public string Path { get; set; }
+
+        public List<string> Discover()
+        {
+            return Directory.GetFiles(Path, FileRules, SearchOption.AllDirectories).ToList();
+        }
+
+        public void Publish()
+        {
+            foreach(var x in Discover())
+            {
+                string fileName, arguments;
+                if (OS.Current == OSType.Windows)
+                {
+                    fileName = "NuGet.exe";
+                    arguments = $"push {x} -s {Address} {ApiKey}";
+                }
+                else
+                {
+                    fileName = "mono";
+                    arguments = $"nuget.exe push {x} -s {Address} {ApiKey}";
+                }
+                Process process = new Process();
+                process.StartInfo = new ProcessStartInfo
+                {
+                    UseShellExecute = false,
+                    RedirectStandardError = true,
+                    RedirectStandardOutput = true,
+                    FileName = fileName,
+                    Arguments = arguments
+                };
+                process.Start();
+                process.WaitForExit();
+            }
+        }
+    }
+}
